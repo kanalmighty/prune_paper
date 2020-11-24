e1ef00e172be6a62663b5d2b36ab33a52005868d
kanalmighty <you@example.com>
2020-11-24 14:36:25 +0800

    订单

---
+++ script/cuda.sh
@@ -0,0 +1,6 @@
+apt-get update && sudo apt-get install -y build-essential git libgfortran3
+wget https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64
+sudo dpkg -i cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64.deb
+sudo apt-key add /var/cuda-repo-<version>/7fa2af80.pub
+sudo apt-get update
+sudo apt-get install cuda
