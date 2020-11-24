86f96c4ba5c033220f9dd18c93014162e85acf2b
kanalmighty <you@example.com>
2020-11-24 18:48:07 +0800

    aa

--- main/prune_train.py
+++ main/prune_train.py
@@ -14,8 +14,8 @@ from torch import nn
 import utils
 from anal_utils import reset_kernel_by_list, get_model, get_conv_name_list, get_net_by_prune_dict, \
     get_conv_idx_by_name, search_by_conv_idx
-from main.resnet34 import resnet_34
-from main.models import *
+from resnet34 import resnet_34
+from .models import *
 from utils import get_loaders
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
